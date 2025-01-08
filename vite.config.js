28dfc295fc4f36a16a0ce609bbf5f01244e00534
JAI <jai@fiveoak.com>
2025-01-08 14:16:00 +0530

    fix: gh-page route v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   base: '/shakti/',
   plugins: [react()],
